b23112d28e61c534f88c7c6fb0810bf8fee1ab0f
ZairiAimenDz <zairiaimendev@gmail.com>
2020-06-19 10:56:07 +0100

    Tests

---
+++ ESIBIB_Student/ESIBIB_Student/Models/User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ESIBIB_Student.Models
+{
+    class User
+    {
+        public string email { get; set; }
+        public string password { get; set; }
+        public List<Request> requestHistory { get; set; }
+    }
+}
